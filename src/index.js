56a53567dddeb8ed8484ed4ad0c6db9e4f53458b
ken <ken.huang@rccchina.com>
2020-01-09 18:05:31 +0800

    v0.1.1

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@
 const path = require('path')
 
 /**
- * @param {string[]} paths - paths to join
+ * @param {...string} paths - paths to join
  * @returns {string} joined path by current directory
+ * @example
+ * 
+ * const join = require('current-path-join')
+ * 
+ * join('../path/to/something')
  */
 module.exports = function (...paths) {
   return path.join(
